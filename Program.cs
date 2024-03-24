db0c5d77dcce86b2cfcf197ed69f5a52af8f965e
hwangminghung <hwangminghung@gmail.com>
2024-03-24 15:13:49 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        DateTime currentTime = DateTime.Now;
+        Console.WriteLine("Thời gian hiện tại của máy tính: " + currentTime);
+    }
+}
